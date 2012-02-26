35a7c2fdf589cabaa57683a388ccbf2f61dfd0dc
Joshua Stupplebeen <jpstup@gmail.com>
2012-02-26 08:49:51 -1000

    Adding and removing files

---
+++ src/Factories.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Dunwich
+{
+    /**
+     * Deprecated in favor of the RVector class which implements the IEnumerable interface
+     * */
+
+    //class RVector : AbstractRVector
+    //{
+    //    int _size = 0;
+    //    string _name = "";
+    //    List<object> _container = new List<object>();
+
+    //    public override int size
+    //    {
+    //        get { return _size; }
+    //        set { _size = value; }
+    //    }
+
+    //    public override string name
+    //    {
+    //        get { return _name; }
+    //        set { _name = value; }
+    //    }
+
+    //    public override List<object> container
+    //    {
+    //        get { return _container; }
+    //        set { _container = value; }
+    //    }
+
+    //    public override void add(object item)
+    //    {
+    //        this.container.Add(item);
+    //    }
+
+    //    public override bool remove(object item)
+    //    {
+    //        try
+    //        {
+    //            this.container.Remove(item);
+    //            return true;
+    //        }
+    //        catch (Exception e)
+    //        {
+    //            Console.WriteLine(e.Message);
+    //            return false;
+    //        }
+    //    }
+    //}
+
+    //class RVectorFactory : AbstractRVectorFactory
+    //{
+    //    public override AbstractRVector GetRVector()
+    //    {
+    //        return new RVector();
+    //    }
+    //}
+
+    class RArrayFactory : RArrayFactoryInf
+    {
+    }
+
+    class RMatrixFactory : RMatrixFactoryInf
+    {
+    }
+}
---
+++ src/RVector.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace Dunwich
+{
+    /**
+     * Class:  RVector
+     * Description:
+     * 
+     * */
+    class RVector : IEnumerable
+    {
+        private List<object> values;
+
+        public RVector()
+        {
+            this.values = new List<object>();
+        }
+
+        /**
+         * Function:    Init
+         * Description: Required in order to grab the C# variable name to be passed to the R File
+         * */
+        public void Init(Expression<Func<RVector>> expr)
+        {
+            var body = ((MemberExpression)expr.Body);
+            //Console.WriteLine("VectorName = {0}", body.Member.Name);
+            new R().WriteCommand(body.Member.Name);
+        }
+
+        /**
+         * Function:  Add
+         * Description:
+         * Parameters:
+         * */
+        public void Add(object value)
+        {
+            this.values.Add(value);
+        }
+
+        /**
+         * Function:    Remove
+         * Description:
+         * Parameters:
+         * */
+        public void Remove(object value)
+        {
+            this.values.Remove(value);
+        }
+
+        // The nested class inheriting the IEnumerator interface is required because the GetEnumerator() method returns
+        // an IEnumerator interface and is required in order for this class to be iterable
+        private class Enum : IEnumerator
+        {
+            List<object> values = null;
+            int position = -1;
+            public Enum(List<object> values)
+            {
+                values = new List<object>();
+            }
+            private IEnumerator GetEnumerator()
+            {
+                return (IEnumerator)this;
+            }
+            public bool MoveNext()
+            {
+                position++;
+                return (position < values.Count());
+            }
+            public void Reset()
+            { position = 0; }
+            public object Current
+            {
+                get
+                {
+                    try
+                    {
+                        return values[position];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+                }
+            }
+        } // End IEnumerator nested class
+
+        public IEnumerator GetEnumerator()
+        {
+            return new Enum(values);
+        }
+
+        public int size
+        {
+            get { return this.values.Count; }
+        }
+
+        public string name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+}
